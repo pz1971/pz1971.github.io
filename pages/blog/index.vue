317419cb861fe38da49f401aba1dd7039ee6020c
Parvez Mia <parvez.pz1971@gmail.com>
2021-07-19 21:31:17 +0600

    blog index design

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
+    <nav-bar location="blog" class="the-nav-bar" />
     <div class="articles">
       <div v-for="article of articles" :key="article" class="article">
         <nuxt-link :to="{ name: 'blog-slug', params: { slug:article.slug } }">
-          <img :src="require(`~/assets/resources/${article.thumbnail}`)" alt="">
+          <img v-if="article.thumbnail!='null.jpg'" :src="require(`~/assets/resources/${article.thumbnail}`)" alt="">
           <div class="detail">
             <h3>{{ article.title }}</h3>
             <p>{{ article.description }}</p>
           </div>
         </nuxt-link>
+        <hr>
       </div>
     </div>
   </div>
@@ -26,6 +28,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.the-nav-bar{
+  margin-left: 10%;
+  margin-right: 10%;
+}
+.articles{
+  margin-left: 10%;
+  margin-right: 10%;
+}
+.article{
+  margin-top: 3ch;
+}
+hr {
+  margin-top: 3ch;
+}
+@media only screen and (max-width: 500px){
+  .the-nav-bar{
+    margin-left: 0%;
+    margin-right: 0%;
+  }
+  .articles{
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+}
 </style>
